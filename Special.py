222f64b8cb0a0630798d538a3a8d302349b7815c
Your Name <your.email@example.com>
2026-08-05 14:12:20 +0530

    Special

---
+++ Special.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def __str__(self):
+        return f"Student(Name={self.name},Marks={self.marks})"
+s=Student("Rahul",90)            
+print(s)
+
+
+
+
+
+
+
+
+
+
